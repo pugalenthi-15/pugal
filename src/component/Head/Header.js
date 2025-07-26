243e3491bd046b49990853d63e8987e8b4bf7e45
pugalenthi-15 <pugalenthi1512@gmail.com>
2025-07-26 12:58:57 +0530

    home update

--- src/component/Head/Header.js
+++ src/component/Head/Header.js
@@ -1,58 +1,50 @@
-import React, { useState } from "react"
-import "./header.css"
+import React, { useState } from "react";
+import "./header.css";
+import info from "../../info.json"
 // import logo from "../pic/logo.png"
 
 const Header = () => {
   // fixed Header
   window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header")
-    header.classList.toggle("active", window.scrollY > 100)
-  })
+    const header = document.querySelector(".header");
+    header.classList.toggle("active", window.scrollY > 100);
+  });
   // Toogle Menu
-  const [Mobile, setMobile] = useState(false)
+  const [Mobile, setMobile] = useState(false);
   return (
     <>
-      <header className='header'>
-        <div className='container d_flex'>
-          <div className='logo'>
+      <header className="header">
+        <div className="container d_flex">
+          <div className="logo">
             {/* <img src="" alt='' /> */}
             <h3>pugal</h3>
           </div>
 
-          <div className='navlink'>
-            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)}>
+          <div className="navlink">
+            <ul
+              className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"}
+              onClick={() => setMobile(false)}
+            >
               {/*<ul className='link f_flex uppercase {Mobile ? "nav-links-mobile" : "nav-links"} onClick={() => setMobile(false)}'>*/}
-              <li>
-                <a href='#home'>home</a>
-              </li>
-              <li>
-                <a href='#features'>features</a>
-              </li>
-              <li>
-                <a href='#portfolio'>portfolio</a>
-              </li>
-              <li>
-                <a href='#resume'>resume</a>
-              </li>
-              <li>
-                <a href='#clients'>clients</a>
-              </li>
-              <li>
-                <a href='#blog'>blog</a>
-              </li>
-              <li>
-                <a href='#contact'>contact</a>
-              </li>
+              {info.nav.map((item, index) => (
+                <li key={index}>
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              ))}
             </ul>
 
-            <button className='toggle' onClick={() => setMobile(!Mobile)}>
-              {Mobile ? <i className='fas fa-times close home-btn'></i> : <i className='fas fa-bars open'></i>}
+            <button className="toggle" onClick={() => setMobile(!Mobile)}>
+              {Mobile ? (
+                <i className="fas fa-times close home-btn"></i>
+              ) : (
+                <i className="fas fa-bars open"></i>
+              )}
             </button>
           </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/component/Hero/Home.js
+++ src/component/Hero/Home.js
@@ -1,68 +1,110 @@
-import React from "react"
-import "./Home.css"
-import hero from "../pic/hero.png"
-import skill1 from "../pic/skill1.png"
-import skill2 from "../pic/skill2.png"
-import skill3 from "../pic/skill3.png"
-import { Typewriter } from "react-simple-typewriter"
+import React from "react";
+import "./Home.css";
+// import hero from "../pic/hero.png"
+import skill1 from "../pic/skill1.png";
+import skill2 from "../pic/skill2.png";
+import skill3 from "../pic/skill3.png";
+import { Typewriter } from "react-simple-typewriter";
+import info from "../../info.json";
 
 const Home = () => {
   return (
     <>
-      <section className='hero' id='home'>
-        <div className='container f_flex top'>
-          <div className='left top'>
+      <section className="hero" id="home">
+        <div className="container f_flex top">
+          <div className="left top">
             <h3>WELCOME TO MY WORLD</h3>
             <h1>
-              Hi, I’m <span>Jone Lee</span>
+              Hi, I’m <span>PUGALENTHI</span>
             </h1>
             <h2>
               a
               <span>
-                <Typewriter words={[" Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+                <Typewriter
+                  words={info.typewrite_content}
+                  loop
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
               </span>
             </h2>
 
-            <p>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
+            <p>{info.description}</p>
 
-            <div className='hero_btn d_flex'>
-              <div className='col_1'>
+            <div className="hero_btn d_flex">
+              <div className="col_1">
                 <h4>FIND WITH ME</h4>
-                <div className='button'>
-                  <button className='btn_shadow'>
-                    <i class='fab fa-facebook-f'></i>
-                  </button>
-                  <button className='btn_shadow'>
-                    <i class='fab fa-instagram'></i>
-                  </button>
-                  <button className='btn_shadow'>
-                    <i class='fab fa-linkedin-in'></i>
-                  </button>
+                <div className="button">
+                  <a
+                    href={info.profile.facebook}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="btn_shadow">
+                      <i className="fab fa-facebook-f"></i>
+                    </button>
+                  </a>
+                  <a
+                    href={info.profile.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="btn_shadow">
+                      <i className="fab fa-instagram"></i>
+                    </button>
+                  </a>
+                  <a
+                    href={info.profile.linkedIn}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button className="btn_shadow">
+                      <i className="fab fa-linkedin-in"></i>
+                    </button>
+                  </a>
                 </div>
               </div>
-              <div className='col_1'>
+              <div className="col_1">
                 <h4>BEST SKILL ON</h4>
-                <button className='btn_shadow'>
-                  <img src={skill1} alt='' />
+                {/* <button className="btn_shadow">
+                  <img src={skill1} alt="" />
                 </button>
-                <button className='btn_shadow'>
-                  <img src={skill2} alt='' />
+                <button className="btn_shadow">
+                  <img src={skill2} alt="" />
                 </button>
-                <button className='btn_shadow'>
-                  <img src={skill3} alt='' />
+                <button className="btn_shadow">
+                  <img src={skill3} alt="" />
+                </button> */}
+                <button className="btn_shadow">
+                  <img src="pugal/images/laravel.png" alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src="pugal/images/django.png" alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src="pugal/images/python.png" alt="" />
+                </button>
+                {/* <button className="btn_shadow">
+                  <img src="/images/skills/mysql.png" alt="" />
+                </button> */}
+                <button className="btn_shadow">
+                  <img src="pugal/images/github.png" alt="" />
                 </button>
               </div>
             </div>
           </div>
-          <div className='right'>
-            <div className='right_img'>
-              <img src={hero} alt='' />
+          <div className="right">
+            <div className="right_img">
+              <img src={info.profile.logo} alt="hai" />
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
